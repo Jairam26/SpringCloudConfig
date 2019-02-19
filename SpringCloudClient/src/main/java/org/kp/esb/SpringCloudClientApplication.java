38dad304968b038b2b0bad9d3963f3d2470160fe
Jayakumar <Jairam26@gmail.com>
2019-02-20 00:31:23 +0530

    test

---
+++ SpringCloudClient/src/main/java/org/kp/esb/SpringCloudClientApplication.java
@@ -0,0 +1,13 @@
+package org.kp.esb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientApplication.class, args);
+	}
+
+}
